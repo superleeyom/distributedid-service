a69cb56b01d80582c2f2ab01540633c29e4003f3
superleeyom <635709492@qq.com>
2021-01-09 16:22:35 +0800

    update

---
+++ src/test/java/com/leeyom/distributedid/DistributedIdServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.leeyom.distributedid;
+
+import com.xiaoju.uemc.tinyid.client.utils.TinyId;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class DistributedIdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Long id = TinyId.nextId("test");
+        // System.out.println(id);
+        // List<Long> ids = TinyId.nextId("test", 10);
+        // System.out.println(ids);
+    }
+
+}
